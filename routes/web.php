0d632a7d5ec42dd58a8b08da441084b253b92a3f
ouchen <medouchen19@gmail.com>
2022-07-10 04:27:37 +0200

    add route dashboard

--- routes/web.php
+++ routes/web.php
@@ -21,9 +21,9 @@ Route::get('/', function () {
    return view('Client.homePage');
 });
 
-// Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
-//     return Inertia::render('Dashboard');
-// })->name('dashboard');
+Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
+    return Inertia::render('Dashboard');
+})->name('dashboard');
 
 // page login for admin
 Route::get('/admin/login', [AuthenticatedSessionController::class, 'create'])->middleware(['guest'])->name('admin.login');
